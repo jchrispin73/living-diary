8ec743691c098c541f7f1101fdcd3505bdc7acb4
jchrispin73 <janinechrispin@gmail.com>
2025-04-01 20:48:44 +0530

    Update living_diary_app.py

--- living_diary_app.py
+++ living_diary_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from home_page import show_home_page
 from gratitude_journal import show_gratitude_journal
 
-# ✅ Must be the very first Streamlit command
+# Must be the very first Streamlit command
 st.set_page_config(
     page_title="Living Diary",
     layout="centered",  # Ensure content is centered
