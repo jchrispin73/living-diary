4c605c4b22926867b3d422925a895cdbdb4af034
jchrispin73 <janinechrispin@gmail.com>
2025-04-02 14:13:09 +0530

    Update living_diary_app.py

--- living_diary_app.py
+++ living_diary_app.py
@@ -39,7 +39,7 @@ with col6:
 st.markdown(
     """
     <div style="text-align: center;">
-        <img src="images/FullLogo_Transparent_NoBuffer.png" style="width: 180px; height: auto; margin: 0 auto;">
+        <img src="FullLogo_Transparent_NoBuffer.png" style="width: 180px; height: auto; margin: 0 auto;">
     </div>
     """, unsafe_allow_html=True
 )
