36df6397a348b5656eae4d343241cc2ae95ef0d1
jchrispin73 <janinechrispin@gmail.com>
2025-04-13 16:29:02 +0530

    Update talk_page.py

--- talk_page.py
+++ talk_page.py
@@ -8,7 +8,7 @@ def show_talk_page():
     <style>
     .stApp {
     background-image: url("https://raw.githubusercontent.com/jchrispin73/living-diary/main/background_talk_v2.png");
-    background-size: 100%;  /* Zoomed out to reveal more of Tara */
+    background-size: 90%;  /* Zoomed out to reveal more of Tara */
     background-position: center;
     background-repeat: no-repeat;
     background-attachment: fixed;
